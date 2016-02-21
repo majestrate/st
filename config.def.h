1f4a4c1ec2748142339f7e4ce46a081bfe7372a6
Jeff <ampernand@gmail.com>
2016-02-21 15:07:09 +0000

    fix out of boundS

--- config.def.h
+++ config.def.h
@@ -108,7 +108,7 @@ static const char *colorname[] = {
  */
 static unsigned int defaultfg = 7;
 static unsigned int defaultbg = 0;
-static unsigned int defaultcs = 256;
+static unsigned int defaultcs = 255;
 
 
 /*
